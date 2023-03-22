c829426b45a99c4f3bc352d0f4031304a9c5d397
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2023-03-22 16:14:53 -0400

    init

---
+++ src/code.r
@@ -0,0 +1,43 @@
+### Name
+# Add your name here
+
+# Please answer the following questions below. If you are writing code, then be sure that your code will run with all necessary libraries. All other responses are to be left as comments in the code.
+
+# Clear all variables
+
+rm(list = ls()) # clear out the variables from memory to make a clean execution of the code.
+
+# If you want to remove all previous plots and clear the console, run the following two lines.
+graphics.off() # clear out all plots from previous work.
+
+cat("\014") # clear the console
+
+
+###########
+# Q 1. Using the iris data set, complete an all-against-all correlation. Hint: you will be using a cor() or corPlot() to run a correlation study of columns; "Sepal.Length", "Sepal.Width",  "Petal.Length", and "Petal.Width".
+
+# TODO: What is the best correlation result from the above test?
+
+
+
+
+###########
+# Q 2. In Regression, saying "regress Y on X" is translated logically as, 
+# Y = a + bX. 
+
+# With this in mind, write code to complete an All-Against-All regression similar to the above correlation test. 
+
+# TODO: Which pair yielded a significant test? Please indicate the significant p-value of the significant test.
+
+
+
+###########
+# Q 3. The Adjusted R-squared value is significant for linear model tests. What Does this value mean? What is the R-squared value for the significant model?
+
+# TODO: 
+
+
+
+
+
+# (Did you remember to write your name above?)
